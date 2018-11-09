97506ad00e81d4a6a6a6abae90c1c3c09b88a1f2
Tapani Heikonen <tapheiko@gmail.com>
2018-11-09 18:57:22 +0200

    TehtavaKolme Commit

---
+++ src/main/java/Tehtava_Kolme/Hello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tehtava_Kolme;
+
+/**
+ *
+ * @author User
+ */
+
+public class Hello {
+
+        public String sayHello()
+        {
+        
+        return sayHello 
+        }
+           
+       
+}
